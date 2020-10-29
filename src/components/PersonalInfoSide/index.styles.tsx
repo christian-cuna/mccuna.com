8ee17877696dff37f06f1abea15a66248c385570
christian-cuna <christian.cuna89@gmail.com>
2020-10-29 22:39:38 +0200

    Update personal info side

--- src/components/PersonalInfoSide/index.styles.tsx
+++ src/components/PersonalInfoSide/index.styles.tsx
@@ -1,28 +1,29 @@
 import styled from 'styled-components';
-import { appBorderStyle } from '../../global/styles';
+import { appBorderStyle, appColors } from '../../global/styles';
 import { mainContainerWidth } from '../Layout/index.styles';
 
 export const S_Container = styled.div`
   border-top: ${appBorderStyle.containerBorder};
   border-bottom: ${appBorderStyle.containerBorder};
   padding: 15px;
-  width: 200px;
+  width: 250px;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: fixed;
-  right: calc(calc(calc(100% - ${mainContainerWidth}) / 2) / 2);
+  right: calc(calc(100% - ${mainContainerWidth}) / 3);
   top: 25%;
   transform: translateX(50%);
 `;
 
 export const S_Img = styled.img`
-  width: 200px;
-  height: 175px;
+  width: 220px;
+  height: 225px;
+  border-radius: 50%;
+  background-color: ${appColors.secondaryColor};
 `;
 
 export const S_Description = styled.p`
-  margin-top: 15px;
   margin-bottom: 0;
   text-align: center;
 `;
--- src/components/PersonalInfoSide/index.tsx
+++ src/components/PersonalInfoSide/index.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { S_Container, S_Description, S_Img } from './index.styles';
-import ProfilePicture from '../../../static/profilePicture.jpg';
+import ProfilePicture from '../../../static/profile.jpg';
 
 export type Props = {
   className?: string;
@@ -9,7 +9,6 @@ export type Props = {
 const PersonalInfoSide: FunctionComponent<Props> = ({ className }) => {
   return (
     <S_Container className={className}>
-      {/* <FontAwesomeIcon icon={faUser} size='9x' /> */}
       <S_Img src={ProfilePicture} />
       <S_Description>
         Short 1st person description in 2-3 lines lorem ipsum dolor sit amet,
